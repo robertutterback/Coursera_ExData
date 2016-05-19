d684221dc6e195ed43f3f1cce06205cdb35e2f94
Robert Utterback <robertu13@gmail.com>
2016-05-19 13:07:33 -0500

    template code for proj2

---
+++ proj2/plot3.R
@@ -0,0 +1,18 @@
+#!/usr/bin/Rscript
+
+## To avoid duplication, util.R contains the code to read in the
+## data. This file is included in the repo, so all plots are still
+## reproducible.
+source("util.R")
+
+main <- function()
+{
+    png(filename = "plot3.png", width=480, height=480)
+    # Code here
+    dev.off()
+}
+
+if (getOption('run.main', default=TRUE)) {
+    options(run.main=FALSE)
+    main()
+}
