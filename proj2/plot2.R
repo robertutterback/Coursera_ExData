ac403a99f82cb843e352b62a6a0ef8b687d1a3ab
Robert Utterback <robertu13@gmail.com>
2016-05-24 19:36:01 -0500

    Added plot2

--- proj2/plot2.R
+++ proj2/plot2.R
@@ -2,18 +2,30 @@
 ## Question 2
 ##
 ## Have total emissions from PM2.5 decreased in the Baltimore City,
-## Maryland (𝚏𝚒𝚙𝚜 == "𝟸𝟺𝟻𝟷𝟶") from 1999 to 2008? Use the base plotting
+## Maryland (fips == "24510") from 1999 to 2008? Use the base plotting
 ## system to make a plot answering this question.
 
-## To avoid duplication, util.R contains the code to read in the
-## data. This file is included in the repo, so all plots are still
-## reproducible.
-source("util.R")
+plot2 <- function(nei, location)
+{
+    ## Sum by year, only for the given location
+    data <- aggregate(Emissions ~ year, sum,
+                      data=subset(nei, fips == location))
+
+    with(data, barplot(Emissions, names.arg=year))
+            
+    ## Line graph alternative:
+    ## with(data, plot(year, Emissions))
+
+
+    title(xlab="Year", ylab="PM2.5 Emissions (tons)",
+          main="PM2.5 Emissions in Baltimore by Year")
+}
 
 main <- function()
 {
     png(filename = "plot2.png", width=480, height=480)
-    # Code here
+    nei <- readRDS("summarySCC_PM25.rds")
+    plot2(nei, "24510")
     dev.off()
 }
 
@@ -21,3 +33,5 @@ if (getOption('run.main', default=TRUE)) {
     options(run.main=FALSE)
     main()
 }
+
+
